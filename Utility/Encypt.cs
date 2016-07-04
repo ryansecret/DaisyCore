3993f9af85160869fc5334c3aa0da539a4fdba26
ryan <shaolinshi111@163.com>
2016-07-04 18:18:57 +0800

    ryan

---
+++ Utility/Encypt.cs
@@ -0,0 +1,23 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+using Daisy.Core.Config;
+
+namespace Daisy.Core.Utility
+{
+    public class Encypt
+    {
+        public static string HashPassWord(string message,string salt)
+        {
+            var md5 = new MD5CryptoServiceProvider();
+            var byt = Encoding.UTF8.GetBytes(salt + message);
+            var bytHash = md5.ComputeHash(byt);
+            md5.Clear();
+            var sTemp = "";
+            for (var i = 0; i < bytHash.Length; i++)
+            {
+                sTemp += bytHash[i].ToString("x").PadLeft(2, '0');
+            }
+            return sTemp;
+        }
+    }
+}
